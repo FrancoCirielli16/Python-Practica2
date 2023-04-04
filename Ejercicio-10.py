012bba4673ff6b775b5d816aa8697356fab623fe
Franco Cirielli <ciriellifrancogiovanni@gmail.com>
2023-04-04 18:51:54 -0300

    ALL

--- Ejercicio-10.py
+++ Ejercicio-10.py
@@ -1,28 +1,30 @@
 
-nombres = ''' 'Agustin', 'Alan', 'Andrés', 'Ariadna', 'Bautista', 'CAROLINA', 'CESAR', 'David',
-'Diego', 'Dolores', 'DYLAN', 'ELIANA', 'Emanuel', 'Fabián', 'Facundo', 'Francsica', 
-'FEDERICO', 'Fernanda', 'GONZALO', 'Gregorio', 'Ignacio', 'Jonathan', 'Joaquina', 'Jorge',
-'JOSE', 'Javier', 'Joaquín'  , 'Julian', 'Julieta', 'Luciana','LAUTARO', 'Leonel', 'Luisa', 
-'Luis', 'Marcos', 'María', 'MATEO', 'Matias', 'Nicolás',  'Nancy', 'Noelia', 'Pablo', 
-'Priscila', 'Sabrina', 'Tomás', 'Ulises', 'Yanina' '''
 
-notas_1 = [81, 60, 72, 24, 15, 91, 12, 70, 29, 42, 16, 3, 35, 67, 10, 57, 11, 69, 12,
-13, 86, 48, 65, 51, 41, 87, 43, 10, 87, 91, 15, 44, 85, 73, 37, 42, 95, 18,
-74, 60, 9, 65, 93, 63, 74]
+nombres = ''' 'Agustin', 'Alan', 'Andrés', 'Ariadna', 'Bautista', 'CAROLINA', 'CESAR',
+'David','Diego', 'Dolores', 'DYLAN', 'ELIANA', 'Emanuel', 'Fabián', 'Facundo',
+'Francsica', 'FEDERICO', 'Fernanda', 'GONZALO', 'Gregorio', 'Ignacio', 'Jonathan',
+'Joaquina', 'Jorge','JOSE', 'Javier', 'Joaquín' , 'Julian', 'Julieta', 'Luciana',
+'LAUTARO', 'Leonel', 'Luisa', 'Luis', 'Marcos', 'María', 'MATEO', 'Matias',
+'Nicolás', 'Nancy', 'Noelia', 'Pablo', 'Priscila', 'Sabrina', 'Tomás', 'Ulises',
+'Yanina' '''
 
-notas_2 = [30, 95, 28, 84, 84, 43, 66, 51, 4, 11, 58, 10, 13, 34, 96, 71, 86, 37, 64,
-87, 14, 14, 49, 27, 55, 69, 77, 59, 57, 40, 96, 24, 30, 73, 95, 19, 47, 15,
-39, 15, 74, 33, 57, 10]
+notas_1 = [81, 60, 72, 24, 15, 91, 12, 70, 29, 42, 16, 3, 35, 67, 10, 57, 11, 69,
+12, 77, 13, 86, 48, 65, 51, 41, 87, 43, 10, 87, 91, 15, 44,
+85, 73, 37, 42, 95, 18, 7, 74, 60, 9, 65, 93, 63, 74]
+
+notas_2 = [30, 95, 28, 84, 84, 43, 66, 51, 4, 11, 58, 10, 13, 34, 96, 71, 86, 37,
+64, 13, 8, 87, 14, 14, 49, 27, 55, 69, 77, 59, 57, 40, 96, 24, 30, 73,
+95, 19, 47, 15, 31, 39, 15, 74, 33, 57, 10]
 
 
 def generar_estrucutra_list(notas_1, notas_2,nombres):
-    nombres = nombres.split(", ")
+    nombres = [nombre.strip() for nombre in nombres.split(',')]
     alumnos = list(zip(nombres, notas_1, notas_2)) #Genero una lista con tublas la cual va a estar 
     return alumnos                              #conformada por el nombre y sus dos notas correspondientes
   
 
 def generar_estrucutra_dict(notas_1, notas_2,nombres):
-    nombres = nombres.split(", ")
+    nombres = [nombre.strip() for nombre in nombres.split(',')]
     alumnos = {}
     for nombre, nota_1, nota_2 in zip(nombres, notas_1, notas_2): #Con zip genero una tupla con el nombre y sus dos notas
         alumnos[nombre] = (nota_1, nota_2)#Y luego en nombre , nota_1 y nota_2 toman sus valores correspondientes para trabajar el diccionario
@@ -45,8 +47,7 @@ def promedio_mas_alto(promedios,nombres):
 
 
 def nota_mas_baja(diccionario):
-    notas_sumadas = {nombre: sum(notas) for nombre, notas in diccionario.items()}
-    nombre_minimo = min(notas_sumadas, key=notas_sumadas.get)
+    nombre_minimo = min(diccionario, key=lambda clave:min(*diccionario.get(clave)))
     return nombre_minimo,min(diccionario[nombre_minimo])
 
 
@@ -77,3 +78,7 @@ if __name__ == "__main__":
     print(f"Nota: {nombre_nota_minima[1]}")
 
     print("-" * 100)
+
+
+
+    
